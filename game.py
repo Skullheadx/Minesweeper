6c97c717cc0e6891e9114c50591377b644a7fae5
Skullheadx <admonty1@gmail.com>
2023-03-09 20:21:36 -0500

    add restart whenever

--- game.py
+++ game.py
@@ -8,13 +8,15 @@ class Game:
 
 	def __init__(self):
 		self.grid = Grid(20, 20, 50)
-
 		self.game_over = False
 
 	def update(self, delta):
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return COMMAND_EXIT
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_r:
+					return COMMAND_RESTART
 
 		mouse_pos = pygame.mouse.get_pos()
 		mouse_button = pygame.mouse.get_pressed()
